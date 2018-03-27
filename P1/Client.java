0859d3c412bd84c0af062ac865e369f4c948fbec
Dylan Katz <muddmaker.97@gmail.com>
2018-03-26 17:07:31 -0700

    add socket boilerplate

--- P1/Client.java
+++ P1/Client.java
@@ -1,9 +1,35 @@
 package P1;
 
+import java.net.*;
+
 class Client
 {
 	public static void main(String[] args)
 	{
-		System.out.printf("Hello, %d\n", args.length);
+		Socket s = null;
+
+		if (args.length != 2) {
+			System.err.println("err: bad args: [host] [port]");
+			System.exit(1);
+		}
+
+		try {
+			s = new Socket(args[0], Integer.parseInt(args[1]));
+		} catch (UnknownHostException e) {
+			System.err.println("err: could not resolve host");
+			System.exit(1);
+		} catch (IllegalArgumentException e) {
+			System.err.println("err: bad port number");
+			System.exit(1);
+		} catch (Exception e) {
+			System.err.printf("err: failed to make socket: %s\n", e);
+			System.exit(1);
+		}
+
+		try {
+			s.close();
+		} catch (Exception e) {
+			System.err.printf("err: failed to kill socket: %s\n", e);
+		}
 	}
 }
--- P1/Server.java
+++ P1/Server.java
@@ -1,9 +1,32 @@
 package P1;
 
+import java.net.*;
+
 class Server
 {
 	public static void main(String[] args)
 	{
-		System.out.printf("Hello, %d\n", args.length);
+		ServerSocket s = null;
+
+		if (args.length != 1) {
+			System.err.println("err: bad args: [port]");
+			System.exit(1);
+		}
+
+		try {
+			s = new ServerSocket(Integer.parseInt(args[0]));
+		} catch (IllegalArgumentException e) {
+			System.err.println("err: bad port number");
+			System.exit(1);
+		} catch (Exception e) {
+			System.err.printf("err: failed to make socket: %s\n", e);
+			System.exit(1);
+		}
+
+		try {
+			s.close();
+		} catch (Exception e) {
+			System.err.printf("err: failed to kill socket: %s\n", e);
+		}
 	}
 }
