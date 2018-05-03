f74c55c1b85c7ba408436e85d7e5cd1269b3289c
Dylan Katz <muddmaker.97@gmail.com>
2018-05-03 13:43:01 -0700

    get it working

--- Lab4/main.cu
+++ Lab4/main.cu
@@ -1,14 +1,43 @@
+/* Copyright (C) 2018 Dylan Katz
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #include <chrono>
 #include <iostream>
 #include <random>
+#include <sstream>
 
+[[noreturn]] void usage(const char **, const char *);
 void random_inputs(float *, float *, int);
 __global__ void add(float *, float *, float *);
 
 int
-main(int argc, char *argv[])
+main(int argc, const char **argv)
 {
-	int N = 32;
+	int N;
+
+	if (argc == 1) {
+		N = 512;
+	} else if (argc == 2) {
+		std::istringstream iss(argv[1]);
+
+		if (!(iss >> N))
+			usage(argv, "first argument must be a number");
+	} else {
+		usage(argv, "gave more than one argument");
+	}
+
 	float *ha, *hb, *hc, *da, *db, *dc;
 
 	ha = (float *)malloc(N * sizeof(float));
@@ -38,8 +67,8 @@ main(int argc, char *argv[])
 	auto end = std::chrono::system_clock::now();
 
 	for (int i = 0; i < N; ++i) {
-		std::cout << "[ " << a[i] << " ] + [ " << b[i];
-		std::cout << " ] = [ " << c[i] << " ]" << std::endl;
+		std::cout << "[ " << ha[i] << " ] + [ " << hb[i];
+		std::cout << " ] = [ " << hc[i] << " ]" << std::endl;
 	}
 
 	free(ha);
@@ -49,6 +78,16 @@ main(int argc, char *argv[])
 	std::chrono::duration<double> dt = end - start;
 	std::cout << "Elapsed time = " << dt.count() << "s" << std::endl;
 	return 0;
+}
+
+[[noreturn]] void
+usage(const char **argv, const char *errmsg)
+{
+	if (errmsg != NULL)
+		std::cerr << "err: " << errmsg << std::endl;
+
+	std::cerr << "usage: " << argv[0] << " [size]" << std::endl;
+	std::exit(1);
 }
 
 void
