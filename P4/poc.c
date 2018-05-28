acd795b22932a239545cf290cea783ed42171576
Dylan Katz <muddmaker.97@gmail.com>
2018-05-27 18:31:16 -0700

    Fix it all

--- P4/poc.c
+++ P4/poc.c
@@ -1,3 +1,18 @@
+/* Copyright (C) 2018 Dylan Katz
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -35,7 +50,7 @@ matmul(double *C, double *A, double *B, int a, int b, int c)
 			C[i*b + j] = 0;
 
 			for(k = 0; k < c; ++k)
-				C[i*b + j] = B[k*b + j] * A[i*c + k];
+				C[i*b + j] += B[k*b + j] * A[i*c + k];
 		}
 	}
 }
